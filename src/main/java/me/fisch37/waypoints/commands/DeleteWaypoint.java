a87302d84394e54003f3f8630f829eb123e83087
Fisch37 <seracin4@gmail.com>
2023-06-30 10:09:21 +0200

    Added permissions support for waypoints.delete.other

--- src/main/java/me/fisch37/waypoints/commands/DeleteWaypoint.java
+++ src/main/java/me/fisch37/waypoints/commands/DeleteWaypoint.java
@@ -3,8 +3,11 @@ package me.fisch37.waypoints.commands;
 import me.fisch37.waypoints.Waypoint;
 import me.fisch37.waypoints.WaypointsMap;
 import org.bukkit.Bukkit;
+import org.bukkit.command.BlockCommandSender;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -67,16 +70,20 @@ public class DeleteWaypoint implements CommandWithHelp, PermissibleCommand {
         }
         ArrayList<String> availableWaypointNames = new ArrayList<>();
         for (Map.Entry<String,Waypoint> item : this.waypoints.entrySet()){
-            if (this.mayDelete(sender,item.getValue())){
+            if (mayDelete(sender,item.getValue())){
                 availableWaypointNames.add(item.getKey());
             }
         }
         return availableWaypointNames;
     }
 
-    public boolean mayDelete(CommandSender sender, Waypoint waypoint){
-        // TODO: Add permissions system
-        return true;
+    public static boolean mayDelete(CommandSender sender, Waypoint waypoint){
+        if (sender instanceof Player player)
+            // Checking for waypoints.delete is redundant
+            return player.hasPermission("waypoints.delete.others") || player.equals(waypoint.getAuthor());
+        // Command Blocks don't get to delete waypoints for security reasons
+        else if (sender instanceof BlockCommandSender) return false;
+        else return sender instanceof ConsoleCommandSender;
     }
 
     @Override
